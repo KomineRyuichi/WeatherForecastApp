c6b1e3911c9211ac9541da0771e84f4e564ac10d
Komine Ryuichi <systena@PCK-135-089-no-MacBook-Pro.local>
2016-09-05 17:05:47 +0900

    no message

---
+++ WeatherForecastApp/MapViewController.m
@@ -0,0 +1,37 @@
+//
+//  MapViewController.m
+//  WeatherForecastApp
+//
+//  Created by PCK-135-087 on 2016/09/05.
+//  Copyright © 2016年 PCK-135-089. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@interface MapViewController ()
+
+@end
+
+@implementation MapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
