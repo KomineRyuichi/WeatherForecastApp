cc57f3205e2a60e8219c953aef2294dfd439a86d
Komine Ryuichi <systena@PCK-135-089-no-MacBook-Pro.local>
2016-09-16 12:18:42 +0900

    その他タブ　実装ファイル作成

---
+++ WeatherForecastApp/OtherTabViewController.h
@@ -0,0 +1,13 @@
+//
+//  OtherTabViewController.h
+//  WeatherForecastApp
+//
+//  Created by PCK-135-089 on 2016/09/16.
+//  Copyright © 2016年 PCK-135-089. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OtherTabViewController : UIViewController
+
+@end
